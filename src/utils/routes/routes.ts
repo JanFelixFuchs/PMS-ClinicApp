d8f67ec5805173a5609a96946568f8ea2d95a6dc
Jan Felix Fuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2025-09-18 00:13:47 +0300

    add: utils file for routes

---
+++ src/utils/routes/routes.ts
@@ -0,0 +1,68 @@
+const appRoutePrefixes = {
+  appointments: 'appointments',
+  clinicians: 'clinicians',
+  devices: 'devices',
+  identity: 'identity',
+  patients: 'patients',
+  rooms: 'rooms',
+};
+
+const pageRoutePrefixes = {
+  appointmentCategories: 'appointmentCategories',
+  appointments: 'appointments',
+
+  clinicianCategories: 'clinicianCategories',
+  clinicians: 'clinicians',
+
+  deviceCategories: 'deviceCategories',
+  devices: 'devices',
+
+  users: 'users',
+  roles: 'roles',
+
+  patients: 'patients',
+
+  roomCategories: 'roomCategories',
+  rooms: 'rooms',
+};
+
+const routes = {
+  // appointments app
+  appointmentCategories: `/${appRoutePrefixes.appointments}/${pageRoutePrefixes.appointmentCategories}`,
+  appointmentCategory: `/${appRoutePrefixes.appointments}/${pageRoutePrefixes.appointmentCategories}/:appointmentCategoryId`,
+  appointments: `/${appRoutePrefixes.appointments}/${pageRoutePrefixes.appointments}`,
+  appointment: `/${appRoutePrefixes.appointments}/${pageRoutePrefixes.appointments}/:appointmentId`,
+
+  // clinicians app
+  clinicianCategories: `/${appRoutePrefixes.clinicians}/${pageRoutePrefixes.clinicianCategories}`,
+  clinicianCategory: `/${appRoutePrefixes.clinicians}/${pageRoutePrefixes.clinicianCategories}/:clinicianCategoryId`,
+  clinicians: `/${appRoutePrefixes.clinicians}/${pageRoutePrefixes.clinicians}`,
+  clinician: `/${appRoutePrefixes.clinicians}/${pageRoutePrefixes.clinicians}/:clinicianId`,
+
+  // devices app
+  deviceCategories: `/${appRoutePrefixes.devices}/${pageRoutePrefixes.deviceCategories}`,
+  deviceCategory: `/${appRoutePrefixes.devices}/${pageRoutePrefixes.deviceCategories}/:deviceCategoryId`,
+  devices: `/${appRoutePrefixes.devices}/${pageRoutePrefixes.devices}`,
+  device: `/${appRoutePrefixes.devices}/${pageRoutePrefixes.devices}/:deviceId`,
+
+  // home app
+  home: '/home',
+
+  // identity app
+  roles: `/${appRoutePrefixes.identity}/${pageRoutePrefixes.roles}`,
+  role: `/${appRoutePrefixes.identity}/${pageRoutePrefixes.roles}/:roleId`,
+  users: `/${appRoutePrefixes.identity}/${pageRoutePrefixes.users}`,
+  user: `/${appRoutePrefixes.identity}/${pageRoutePrefixes.users}/:userId`,
+
+  // patients app
+  patients: `/${appRoutePrefixes.patients}/${pageRoutePrefixes.patients}`,
+  patient: `/${appRoutePrefixes.patients}/${pageRoutePrefixes.patients}/:patientId`,
+
+  // rooms app
+  roomCategories: `/${appRoutePrefixes.rooms}/${pageRoutePrefixes.roomCategories}`,
+  roomCateogry: `/${appRoutePrefixes.rooms}/${pageRoutePrefixes.roomCategories}/:roomCategoryId`,
+  rooms: `/${appRoutePrefixes.rooms}/${pageRoutePrefixes.rooms}`,
+  room: `/${appRoutePrefixes.rooms}/${pageRoutePrefixes.rooms}/:roomId`,
+};
+
+export default routes;
