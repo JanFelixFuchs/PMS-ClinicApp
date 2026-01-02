e132971a5bc4d49ec5a932d700cfd6e2e8a50f63
Jan Felix Fuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-01-02 22:55:41 +0100

    add: providers for redux store, i18n & antd

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,20 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import './main.css';
+import { Provider } from 'react-redux';
+import configureStore from './utils/store/configureStore';
+import { I18nextProvider } from 'react-i18next';
+import i18n from './utils/config/i18next';
+import AntdProvider from './components/config/AntdProvider';
+const store = configureStore();
 
-createRoot(document.getElementById('root')!).render(
-  <>
-  </>,
-)
+const container = document.getElementById('root')!;
+const root = createRoot(container);
+
+root.render(
+  <Provider store={store}>
+    <I18nextProvider i18n={i18n}>
+        <AntdProvider>
+        </AntdProvider>
+    </I18nextProvider>
+  </Provider>
+);
