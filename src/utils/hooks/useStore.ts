534c3b1ac1694cc477992c181f43967b44cf74b5
Jan Felix Fuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-01-26 16:56:27 +0200

    remove: custom useStore hook

--- src/utils/hooks/useStore.ts
+++
@@ -1,6 +0,0 @@
-import { useStore as useStoreRedux } from 'react-redux';
-import type { AppStore } from '../store/configureStore';
-
-const useStore = useStoreRedux.withTypes<AppStore>();
-
-export default useStore;
