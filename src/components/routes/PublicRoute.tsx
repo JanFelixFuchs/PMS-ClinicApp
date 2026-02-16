52f61f5b62cecbaad6bf7e149ad201ae98ae822f
Jan Felix Fuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-02-16 22:07:07 +0200

    add: route component for public pages

---
+++ src/components/routes/PublicRoute.tsx
@@ -0,0 +1,33 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import routes from '../../utils/routes/routes';
+import useSelector from '../../utils/hooks/useSelector';
+import { selectIsAuthenticated } from '../../utils/store/slices/identity';
+
+/* - - - STATIC ELEMENTS - - - */
+
+/* - - - COMPONENT - - - */
+const PublicRoute = () => {
+  /* - - - DESTRUCTING - - - */
+  const isAuthenticated = useSelector(selectIsAuthenticated);
+
+  /* - - - STATE VARIABLES - - - */
+
+  /* - - - VARIABLES - - - */
+
+  /* - - - FUNCTIONS - - - */
+
+  /* - - - USE EFFECTS - - - */
+
+  /* - - - RETURN - - - */
+  if (isAuthenticated)
+    return (
+      <Navigate
+        to={routes.home}
+        replace
+      />
+    );
+
+  return <Outlet />;
+};
+
+export default PublicRoute;
