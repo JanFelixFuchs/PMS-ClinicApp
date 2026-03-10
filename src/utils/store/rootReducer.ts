b64a131a7ee435bf61616c57963e3f4f9d164f00
Jan Felix Fuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-03-10 23:41:30 +0200

    add: reducers for entities

--- src/utils/store/rootReducer.ts
+++ src/utils/store/rootReducer.ts
@@ -1,8 +1,10 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import identityReducer from '../store/slices/identity';
 import uiReducer from '../store/slices/ui';
+import entitiesReducer from './entitiesReducer';
 
 const rootReducer = combineReducers({
+  entities: entitiesReducer,
   identity: identityReducer,
   ui: uiReducer,
 });
