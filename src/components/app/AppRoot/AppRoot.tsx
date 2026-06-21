2dad08090431f1e91cb688015ef02dd897d3042c
Jan Felix Fuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-06-21 03:23:07 +0300

    add: app root component with automatic login attempt

---
+++ src/components/app/AppRoot/AppRoot.tsx
@@ -0,0 +1,74 @@
+import { Route, Routes } from 'react-router-dom';
+import PublicRoute from '../../routes/PublicRoute';
+import routes from '../../../utils/routes/routes';
+import RegistrationPage from '../../../pages/registration/RegistrationPage';
+import LoginPage from '../../../pages/login/LoginPage';
+import ProtectedRoute from '../../routes/ProtectedRoute';
+import AppContainer from '../AppContainer/AppContainer';
+import { App } from 'antd';
+import useDispatch from '../../../utils/hooks/useDispatch';
+import { useRefreshTokensMutation } from '../../../utils/store/slices/api/auth';
+import { useEffect, useState } from 'react';
+import { login } from '../../../utils/store/slices/identity';
+import styles from './AppRoot.module.css';
+
+/* - - - STATIC ELEMENTS - - - */
+
+/* - - - COMPONENT - - - */
+const AppRoot = () => {
+  /* - - - DESTRUCTING - - - */
+  const dispatch = useDispatch();
+  const [refreshTokens] = useRefreshTokensMutation();
+
+  /* - - - STATE VARIABLES - - - */
+  const [isInitialized, setIsInitialized] = useState<boolean>(false);
+
+  /* - - - VARIABLES - - - */
+
+  /* - - - FUNCTIONS - - - */
+
+  /* - - - USE EFFECTS - - - */
+  useEffect(() => {
+    const initialize = async () => {
+      try {
+        const result = await refreshTokens().unwrap();
+        dispatch(login({ ...result }));
+      } catch {
+        // user remains unauthenticated since no valid refresh token is present
+      } finally {
+        setIsInitialized(true);
+      }
+    };
+
+    initialize();
+  }, [dispatch, refreshTokens]);
+
+  /* - - - RETURN - - - */
+  if (!isInitialized) return null;
+
+  return (
+    <App className={styles['antDesign-app'] ?? ''}>
+      <Routes>
+        <Route element={<PublicRoute />}>
+          <Route
+            path={routes.login}
+            element={<LoginPage />}
+          />
+          <Route
+            path={routes.registration}
+            element={<RegistrationPage />}
+          />
+        </Route>
+
+        <Route element={<ProtectedRoute />}>
+          <Route
+            path='*'
+            element={<AppContainer />}
+          />
+        </Route>
+      </Routes>
+    </App>
+  );
+};
+
+export default AppRoot;
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import './main.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import AppContainer from './components/app/AppContainer/AppContainer';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import configureStore from './utils/store/configureStore';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './utils/config/i18next';
-import LoginPage from './pages/login/LoginPage';
-import RegistrationPage from './pages/registration/RegistrationPage';
-import ProtectedRoute from './components/routes/ProtectedRoute';
-import routes from './utils/routes/routes';
 import AntdProvider from './components/config/AntdProvider';
-import PublicRoute from './components/routes/PublicRoute';
+import AppRoot from './components/app/AppRoot/AppRoot';
 
 const store = configureStore();
 
@@ -23,25 +18,7 @@ root.render(
     <I18nextProvider i18n={i18n}>
       <BrowserRouter>
         <AntdProvider>
-          <Routes>
-            <Route element={<PublicRoute />}>
-              <Route
-                path={routes.login}
-                element={<LoginPage />}
-              />
-              <Route
-                path={routes.registration}
-                element={<RegistrationPage />}
-              />
-            </Route>
-
-            <Route element={<ProtectedRoute />}>
-              <Route
-                path='*'
-                element={<AppContainer />}
-              />
-            </Route>
-          </Routes>
+          <AppRoot />
         </AntdProvider>
       </BrowserRouter>
     </I18nextProvider>
