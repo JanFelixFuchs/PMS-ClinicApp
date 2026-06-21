47e478764fbb9c86d78b68916f18c14b2d6db909
Jan Felix Fuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-06-22 00:27:49 +0300

    add: sidebar status to ui slice

--- src/utils/store/slices/ui.ts
+++ src/utils/store/slices/ui.ts
@@ -1,13 +1,16 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from '../configureStore';
-import type { Language } from '../../types/ui/Languages';
+import type { Language } from '../../types/ui/Language';
+import { SidebarStatus } from '../../types/ui/SidebarStatus';
 
 interface UiState {
   language: Language;
+  sidebarStatus: SidebarStatus;
 }
 
 const initialState: UiState = {
   language: 'en',
+  sidebarStatus: 'collapsed',
 };
 
 const ui = createSlice({
@@ -18,11 +21,21 @@ const ui = createSlice({
     changeLanguage: (state, action: PayloadAction<{ language: Language }>) => {
       state.language = action.payload.language;
     },
+
+    // reducer for changing the sidebar status
+    changeSidebarStatus: (
+      state,
+      action: PayloadAction<{ sidebarStatus: SidebarStatus }>
+    ) => {
+      state.sidebarStatus = action.payload.sidebarStatus;
+    },
   },
 });
 
-export const { changeLanguage } = ui.actions;
+export const { changeLanguage, changeSidebarStatus } = ui.actions;
 
 export const selectLanguage = (state: RootState) => state.ui.language;
+
+export const selectSidebarStatus = (state: RootState) => state.ui.sidebarStatus;
 
 export default ui.reducer;
