05226f7a5bac257d4cc18e1a2e77fa62dcdef008
Jan Felix Fuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-06-22 00:28:46 +0300

    add: rtk tags

---
+++ src/utils/api/rtkTagTypes.ts
@@ -0,0 +1,19 @@
+export const rtkTagTypes = {
+  appointmentCategory: 'AppointmentCategory',
+  appointmentProtocol: 'AppointmentProtocol',
+  appointment: 'Appointment',
+  clinicianCategory: 'ClinicianCategory',
+  clinician: 'Clinician',
+  deviceCategory: 'DeviceCategory',
+  device: 'Device',
+  patient: 'Patient',
+  result: 'Result',
+  role: 'Role',
+  room: 'Room',
+  roomCategory: 'RoomCategory',
+  user: 'User',
+} as const;
+
+export const rtkIdTypes = {
+  list: 'LIST',
+};
