8bd8994ce4ab00e1847e3212bf0a1f12ce5a14d1
Jan Felix Fuchs <123419057+JanFelixFuchs@users.noreply.github.com>
2026-06-23 00:14:14 +0300

    update: identity slice to identity output models

--- src/utils/store/slices/identity.ts
+++ src/utils/store/slices/identity.ts
@@ -1,106 +1,70 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
 import type { RootState } from '../configureStore';
-import { apiCallBegan } from '../middleware/apiActionCreator';
-import { Protocol } from '../../types/utils/Protocol';
-import { HttpMethod } from '../../types/utils/HttpMethod';
+import type { CurrentUserOutputModel } from '../../types/outputModels/SharedOutputModels';
+import type { ClinicOutputModel } from '../../types/outputModels/ClinicOutputModels';
 
 interface IdentityState {
-  user: {
-    firstName: string | null;
-    lastName: string | null;
-  };
-  clinic: {
-    clinicId: string | null;
-    name: string | null;
-    abbreviation: string | null;
-    owner: string | null;
-    medicalField: string | null;
-    email: string | null;
-    phoneNumber: string | null;
-    street: string | null;
-    houseNumber: string | null;
-    city: string | null;
-    zipCode: string | null;
-  };
-  auth: {
-    accessToken: string | null;
-    refreshToken: string | null;
-    isAuthenticated: boolean;
-  };
+  clinic: ClinicOutputModel | null;
+  currentUser: CurrentUserOutputModel | null;
+  accessToken: string | null;
 }
 
 const initialState: IdentityState = {
-  user: {
-    firstName: 'PlaceholderFirstName',
-    lastName: null,
-  },
-  clinic: {
-    clinicId: null,
-    name: 'PlaceholderClinicName',
-    abbreviation: null,
-    owner: null,
-    medicalField: null,
-    email: null,
-    phoneNumber: null,
-    street: null,
-    houseNumber: null,
-    city: null,
-    zipCode: null,
-  },
-  auth: {
-    accessToken: null,
-    refreshToken: null,
-    isAuthenticated: true,
-  },
+  clinic: null,
+  currentUser: null,
+  accessToken: null,
 };
 
 const identity = createSlice({
   name: 'identity',
   initialState,
   reducers: {
-    // reducer for login success
-    loginSucceeded: (
+    // reducer for login
+    login: (
       state,
-      action: PayloadAction<{ accessToken: string; refreshToken: string }>
+      action: PayloadAction<{
+        user: CurrentUserOutputModel;
+        clinic: ClinicOutputModel;
+        accessToken: string;
+      }>
     ) => {
-      state.auth.accessToken = action.payload.accessToken;
-      state.auth.refreshToken = action.payload.refreshToken;
-      state.auth.isAuthenticated = true;
+      state.clinic = action.payload.clinic;
+      state.currentUser = action.payload.user;
+      state.accessToken = action.payload.accessToken;
     },
 
-    // reducer for login failure
-    loginFailed: (state) => {
-      state.auth.accessToken = null;
-      state.auth.refreshToken = null;
-      state.auth.isAuthenticated = false;
+    // reducer for logout
+    logout: () => {
+      return initialState;
     },
 
-    // test reducer
-    loginSu: (state) => {
-      state.auth.isAuthenticated = true;
+    // reducer for setting an access token
+    setAccessToken: (state, action: PayloadAction<{ accessToken: string }>) => {
+      state.accessToken = action.payload.accessToken;
+    },
+
+    // reducer for setting clinic
+    setClinic: (
+      state,
+      action: PayloadAction<{ clinic: ClinicOutputModel }>
+    ) => {
+      state.clinic = action.payload.clinic;
     },
   },
 });
 
-export const { loginSu } = identity.actions;
-
-// action creator for login
-export const login = () =>
-  apiCallBegan({
-    protocol: Protocol.HTTPS,
-    url: '/asaksak',
-    method: HttpMethod.GET,
-    onStart: loginSu.type,
-  });
+export const { login, logout, setAccessToken, setClinic } = identity.actions;
 
 // selector for user
-export const selectUser = (state: RootState) => state.identity.user;
+export const selectUser = (state: RootState) => state.identity.currentUser;
 
 // selector for clinic
 export const selectClinic = (state: RootState) => state.identity.clinic;
 
-// selector for isAuthenticated
+// selector for authentication status
 export const selectIsAuthenticated = (state: RootState) =>
-  state.identity.auth.isAuthenticated;
+  state.identity.currentUser !== null &&
+  state.identity.clinic !== null &&
+  state.identity.accessToken !== null;
 
 export default identity.reducer;
